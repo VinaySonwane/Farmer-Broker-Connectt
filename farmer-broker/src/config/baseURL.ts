740bacdc54c8d1eedfa99325bf4aa99460d34630
https://github.com/VinaySonwane <sonwanevinay11@gmail.com>
2025-07-07 14:53:57 +0530

    updated the bseURl

--- farmer-broker/src/config/baseURL.ts
+++ farmer-broker/src/config/baseURL.ts
@@ -5,7 +5,7 @@ let baseUrl: string = "";
 if (process.env.NODE_ENV === "development") {
   baseUrl = "http://localhost:8000"; // Local backend during development
 } else {
-  baseUrl = "https://fasaltrade.vercel.app"; // Your production backend URL
+  baseUrl = "https://farmer-broker-connectt.vercel.app"; // Your production backend URL
 }
 
 export default baseUrl;
