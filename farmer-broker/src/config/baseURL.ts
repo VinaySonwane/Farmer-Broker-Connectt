f24598dfb7733ebb06330a4625879cb8eaf16459
https://github.com/VinaySonwane <sonwanevinay11@gmail.com>
2025-07-09 13:00:20 +0530

    changed the Server

--- farmer-broker/src/config/baseURL.ts
+++ farmer-broker/src/config/baseURL.ts
@@ -5,7 +5,8 @@ let baseUrl: string = "";
 if (process.env.NODE_ENV === "development") {
   baseUrl = "http://localhost:8000"; // Local backend during development
 } else {
-  baseUrl = "https://farmer-broker-connectt.vercel.app"; // Your production backend URL
+  baseUrl = "https://farmer-backend-1-buqj.onrender.com"; // Your production backend URL
 }
-
+//Vercel Backend
+// https://farmer-broker-connectt.vercel.app
 export default baseUrl;
